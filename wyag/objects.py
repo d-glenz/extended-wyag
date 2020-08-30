e222424ddf79c8fb2b0a89cbc8ed671be8b4494b
Dominik <doglenz@web.de>
2020-08-31 01:20:29 +0200

    Need to add support to branches/tags to object_resolve #10

--- wyag/objects.py
+++ wyag/objects.py
@@ -335,6 +335,11 @@ def object_resolve(repo: GitRepository, name: str) -> List[str]:
                 if str(f).startswith(rem):
                     candidates.append(prefix + str(f))
 
+    # search for branches and tags (with or without "refs" and "heads" or "tags" prefixes)
+    for path in [f'refs/heads/{name}', f'refs/tags/{name}', f'refs/{name}', name]:
+        if repo_file(repo, path).exists():
+            candidates.append(ref_resolve(repo, path))
+
     return candidates
 
 
